164a7c26e627967dced711fe4853d7680862ef55
jwelch31 <josh_welch@infooptions.com>
2019-09-27 19:10:59 -0600

    -partial implementation of db

--- Congo Chess/src/Database/DB.java
+++ Congo Chess/src/Database/DB.java
@@ -6,7 +6,7 @@ public class DB {
 	
 	//JDBC driver and database
 	private static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
-	private static final String DB_URL = "jdbc:mariadb://faure:3306/javathehutt";
+	private static final String DB_URL = "jdbc:mariadb://faure/javathehutt";
 	
 	//login credentials
 	private static final String USER = "jwelch31";
@@ -14,6 +14,8 @@ public class DB {
 			
 	public static boolean isUser(String user, String pass){
 		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
 			//register driver
@@ -22,12 +24,16 @@ public class DB {
 			//open connection
 			conn = generateConnection();
 			
+			//create statement
+			stmt = conn.createStatement();
+			
 			//create the query
 			String sql = "select Username, Password from LoginInformation"
 					+ "where (Username ==" + USER;
 			
 			//Execute the query and store the result
-			ResultSet rs = generateStatement(conn, sql);
+			//rs = generateStatement(stmt, sql);
+			
 			if (rs.getString("Username").contentEquals(user) && rs.getString("Password").contentEquals(pass)) {
 				System.out.println("here");
 				return true;
@@ -39,16 +45,32 @@ public class DB {
 			System.out.println(se);
 		} catch(Exception e) {
 			System.out.println(e);
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) { /* ignored */}
+				
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) { /* ignored */}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) { /* ignored */}
+			}
 		}
 		return false;
 	}
 	
-	public static Connection generateConnection() throws Exception{
+	private static Connection generateConnection() throws Exception{
 		return DriverManager.getConnection(DB_URL, USER, PASSWORD);
 	}
 	
-	public static ResultSet generateStatement(Connection conn, String sql)throws Exception{
-		Statement stmt = conn.createStatement();
+	private static ResultSet generateStatement(Statement stmt, String sql)throws Exception{
 		return stmt.executeQuery(sql);
 	}
 }
