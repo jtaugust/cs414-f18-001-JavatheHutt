a9a945e88ec448314e087ec90e8143a64abababf
TylerDansby <tsdansby@rams.colostate.edu>
2019-09-29 17:16:10 -0600

    New user is now added to file.

--- Congo Chess/src/Screens/RegistrationScreen.java
+++ Congo Chess/src/Screens/RegistrationScreen.java
@@ -16,7 +16,7 @@ import GUI.Label;
 import GUI.Panel;
 
 public class RegistrationScreen {
-	private static boolean loginError = false;
+	private static boolean registrationError = false;
 	public static void screen(){
 		//Set current screen to Registration
 		Application.setCurrentScreen("Registration");
@@ -28,9 +28,9 @@ public class RegistrationScreen {
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 		//build the panel
-		JTextField email = Helpers.newTextField("email", "temp@fake.com");
+		JTextField emailfield = Helpers.newTextField("email", "temp@fake.com");
 		panel.add(Helpers.spacer(100, 50));
-		panel.add(email);
+		panel.add(emailfield);
 
 		//create space under email textfield
 		panel.add(Helpers.spacer(200, 50));
@@ -41,22 +41,23 @@ public class RegistrationScreen {
 		//create space under username textfield
 		panel.add(Helpers.spacer(200, 50));
 	
-		JTextField password = Helpers.newTextField("Password", "tempPass");
-	    panel.add(password);
+		JTextField passwordField = Helpers.newTextField("Password", "tempPass");
+	    panel.add(passwordField);
 	    
 		//create space under password textfield
 		panel.add(Helpers.spacer(200, 50));
 	    
 	    JTextField passwordConfirm = Helpers.newTextField("Confirm Password", "tempPass");
 	    panel.add(passwordConfirm);
+	    
 		
 	    //create the panel below password
 	    JPanel error = new JPanel();
 	    error.setOpaque(false);
 	    error.setMaximumSize(new Dimension(400,50));
-	    if (loginError == true){ //if login failed, update panel to an error label
+	    if (registrationError == true){ //if login failed, update panel to an error label
 	    	error.setOpaque(false);
-	    	error.add(Label.errorLabel("<html>Passwords do not match<html>", Color.red));
+	    	error.add(Label.errorLabel("<html>Error while trying to register<html>", Color.red));
 	    }
 	    panel.add(error);
 	    
@@ -67,8 +68,29 @@ public class RegistrationScreen {
 				new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						//TODO: create new user in database if name and email isn't already taken.
-						Application.changeScreen("InitialMain");
+						String name = username.getText();
+						String email = emailfield.getText();
+						String password = passwordField.getText();
+						String passwordCon = passwordConfirm.getText();
+						if(!password.equals(passwordCon)){ // passwords don't match
+							setRegistrationError();
+						} else {
+							//if passwords match, make sure name or email isn't already taken.
+							if (!Database.DB.isTaken(name, email)){ // user or email isn't taken, add new user to file.
+								Database.DB.createUser(name, password, email);
+								Application.setUser(name);
+								Application.changeScreen("InitialMain");
+							}else{ // user or email is taken
+								//update application variable "User" to username.getText()
+								RegistrationScreen.setRegistrationError();
+								Application.changeScreen("Registration");
+								//redirect to main page
+				
+							}
+							
+						}
+						
+			
 						
 					}
 				}
@@ -89,13 +111,13 @@ public class RegistrationScreen {
 		panel.add(register);
 		
 		//if loginError was set, change it back
-		if (loginError == true){
-			loginError = false;
+		if (registrationError == true){
+			registrationError = false;
 		}
 	}
 	
-	public static void setLoginError(){
-		loginError = true;
+	public static void setRegistrationError(){
+		registrationError = true;
 	}
 	
 }
