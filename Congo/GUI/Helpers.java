63a717a7cdb7a401ae6571fc423eb8b382837e5b
jwelch31 <josh_welch@infooptions.com>
2019-10-03 19:00:48 -0600

    update helpers

--- Congo/GUI/Helpers.java
+++ Congo/GUI/Helpers.java
@@ -12,7 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 
 public class Helpers {
-	
+
 	//gets the requested image from its location and returns it
 	public static BufferedImage getImage(String location){
 		BufferedImage image = null;
@@ -23,7 +23,7 @@ public class Helpers {
 		}
 		return image;
 	}
-	
+
 	//resizes an image with the provided size dimension
 	public static BufferedImage resizeImg(BufferedImage img, Dimension size) {
 		int width = size.width, height = size.height;
@@ -34,7 +34,7 @@ public class Helpers {
 	    g2d.dispose();
 	    return newImage;
 	}
-	
+
 	//creates a JTextField with the name, fills the field with text, sets the size, and sets the font of the text
 	public static JTextField textField(String name, String text, Dimension size, Font font) {
 		JTextField field = new JTextField(text);
@@ -48,23 +48,23 @@ public class Helpers {
 
 		return field;
 	}
-	
+
 	//creates a blank box for spacing panel content
 	public static Component spacer(int width, int height) {
 		return Box.createRigidArea(new Dimension(width, height));
 	}
-	
+
 	//creates a new bolded font of SansSerif, of proveded size
 	public static Font newFont(int fontSize) {
 		return new Font("SansSerif", Font.BOLD, fontSize);
 	}
-	
+
 	//create a button with the provided name
 	public static JButton button(String name) {
 		JButton button = new JButton(name);
 		return button;
 	}
-	
+
 	//create a button with the provided name of the provided size
 	public static JButton button(String name, Dimension size) {
 		JButton button = button(name);
@@ -72,7 +72,7 @@ public class Helpers {
 		button.setAlignmentX(Component.CENTER_ALIGNMENT);
 		return button;
 	}
-	
+
 	//create a button with an image instead of text, with the provided size
 	public static JButton button(BufferedImage img, Dimension imgSize) {
 		JButton button = new JButton();
@@ -80,19 +80,29 @@ public class Helpers {
 		button.setIcon(new ImageIcon(img));
 		return button;
 	}
-	
-	//creates a textField with the given name, will have no text, with a width of 200, height of 50, and a font size of 22
-	public static JTextField newTextField(String name){
-		JTextField field = textField(name,"", new Dimension(200, 50), newFont(22));
+
+
+	public static JTextField newTextField(String name, String text){ //creates a textField with ID as given name, fills in the field with the text given,width of 200, height of 50, and font size of 22
+		JTextField field = textField(name, text, new Dimension(400, 50), newFont(22));
+		//clear default fields
+		field.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				if(field.getText().equals(text)) {
+					field.setText(""); 
+				}
+			}
+			public void focusLost(FocusEvent e) {
+				if(field.getText().isEmpty()) {
+					field.setText(text);
+
+				}
+
+			}
+		}); 
+
 		return field;
 	}
 
-	 //creates a textField with the name, fills in the field with the text given,width of 200, height of 50, and font size of 22
-	public static JTextField newTextField(String name, String text){
-		JTextField field = textField(name, text, new Dimension(200, 50), newFont(22));
-		return field;
-	}
-	
 	//creates a textField with the name, fills the field with text, sets the size, and defaults to a font size of 22
 	public static JTextField newTextField(String name, String text, Dimension size){
 		JTextField field = textField(name,text, size, newFont(22));
