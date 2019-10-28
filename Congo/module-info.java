b4a07c20bd9cff5cf33576fde031b512809f4554
Jacob Augustine <Jacob Augustine@DESKTOP-R8KMNTV>
2019-10-28 10:13:30 -0600

    Added Server helper methods for Users. Games soon to come.

---
+++ Congo/module-info.java
@@ -0,0 +1,13 @@
+module Project {
+	exports App;
+	exports BoardLogic;
+	exports Database;
+	exports Templates;
+	exports GUI;
+	exports Screens;
+	exports Tests;
+
+	requires java.desktop;
+	requires java.sql;
+	requires junit;
+}
