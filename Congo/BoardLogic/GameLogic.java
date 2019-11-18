d10f8fe39e07825ed1938199231fa396698bee8b
Tyler Dansby <42559191+TylerDansby@users.noreply.github.com>
2019-11-17 19:21:21 -0700

    Revert "Refactor Game Logic and Tests"

--- Congo/BoardLogic/GameLogic.java
+++ Congo/BoardLogic/GameLogic.java
@@ -1,5 +1,4 @@
 package BoardLogic;
-
 //
 // GameLogic.java 
 // 
@@ -409,42 +408,16 @@ public class GameLogic extends State{
 		String[][] board = state.getBoard();
 		
 		// Set to 15 for max amount of moves 
-		String[] allPossibleMoves = new String[20];
+		String[] allPossibleMoves = new String[10];
 		int count = 0; 
 		
-		// Check for river 
+		// Check for river moves
 		if(i == 3) {
-			
-			// Check Left 
-			int riverCounter = j - 1;
-			while(riverCounter >= 0){
-				if(board[i][riverCounter].charAt(2) == 'N'){
-					allPossibleMoves[count] = board[i][riverCounter];
+			for(int y = 0; y < 6; y++) {
+				if(board[i][y].charAt(2) == 'N' || board[i][y].charAt(2) != state.getCurrentTurnColor().charAt(0)) {
+					allPossibleMoves[count] = board[i][y];
 					count++;
-				}else if (board[i][riverCounter].charAt(2) != state.getCurrentTurnColor().charAt(0)){
-					allPossibleMoves[count] = board[i][riverCounter];
-					count++;
-					break;
-				}else{
-					break;
 				}
-				riverCounter--;
-			}
-
-			// Check Right 
-			riverCounter = j + 1;
-			while(riverCounter <= 6){
-				if(board[i][riverCounter].charAt(2) == 'N'){
-					allPossibleMoves[count] = board[i][riverCounter];
-					count++;
-				}else if (board[i][riverCounter].charAt(2) != state.getCurrentTurnColor().charAt(0)){
-					allPossibleMoves[count] = board[i][riverCounter];
-					count++;
-					break;
-				}else{
-					break;
-				}
-				riverCounter++;
 			}
 		}
 		
@@ -479,7 +452,7 @@ public class GameLogic extends State{
 			}
 			allPossibleMoves[count] = board[3][j];
 			count++;
-		}else if(i < 3){
+		}else {
 			int riverCounter = i + 1;
 			while( riverCounter != 3) {
 				if((board[riverCounter][j].charAt(2) != 'N')){
