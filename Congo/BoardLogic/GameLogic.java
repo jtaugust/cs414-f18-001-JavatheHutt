7ec0c12a925dd3490e9c9360a2a74492a6545099
nkaliher <nickkaliher@gmail.com>
2019-12-05 14:52:01 -0700

    Adding more refactored tests

--- Congo/BoardLogic/GameLogic.java
+++ Congo/BoardLogic/GameLogic.java
@@ -10,38 +10,9 @@ package BoardLogic;
 public class GameLogic extends State{
 	State state;
 
-	// Constructor used to start game logic upon creation of an object
 	public GameLogic() {
 		//mainLogic(state);
 	}
-
-	// Check if two boards are equal
-    public boolean boardEquals(Piece[][] board1, Piece[][] board2){
-        // Return False if not equal
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 7; j++){
-                if(board1[i][j] == null){
-                    // If both spots not null
-                    if(board2[i][j] != null){
-                        return false;
-                    }
-                } else {
-                    // Check Color
-                    if(board1[i][j].getColor() != board2[i][j].getColor()){
-                        return false;
-                    }
-
-                    // Check piece type
-                    if(board1[i][j].getType() != board2[i][j].getType()){
-                        return false;
-                    }
-                }
-            }
-        }
-        
-        // Boards are equal
-        return true;
-    }
 	
 	// Flips board to ensure same logic for both sides 
 	public Piece[][] flipBoard(State state){
@@ -83,174 +54,197 @@ public class GameLogic extends State{
 		return newBoard;
 	}
 		
-	// Update the GUI with all the possible moves 
-	public int[][] displayPossibleMoves(State state){
+// 	// Update the GUI with all the possible moves 
+// 	public int[][] displayPossibleMoves(State state){
 		
-		int[][] moveArray = new int[20][2];
-		int row = state.getPieceSelected().getRow();
-		int col = state.getPieceSelected().getColumn();
+// 		int[][] moveArray = new int[20][2];
+// 		int row = state.getPieceSelected().getRow();
+// 		int col = state.getPieceSelected().getColumn();
 
-		// Determine which piece to return moves for
-		switch(state.getPieceSelected().getType()) {
-			case 'G':
-				Giraffe giraffe = new Giraffe(row,col,state.getCurrentTurnColor(),'G');
-				return giraffe.legalMoves(state);
+// 		// Determine which piece to return moves for
+// 		switch(state.getPieceSelected().getType()) {
+// 			case 'G':
+// 				Giraffe giraffe = new Giraffe(row,col,state.getCurrentTurnColor(),'G');
+// 				return giraffe.legalMoves(state);
 				
-			case 'M':
-				Monkey monkey = new Monkey(row,col,state.getCurrentTurnColor(),'M');
-				return monkey.legalMoves(state);
+// 			case 'M':
+// 				Monkey monkey = new Monkey(row,col,state.getCurrentTurnColor(),'M');
+// 				return monkey.legalMoves(state);
 					
-			case 'E':
-				Elephant elephant = new Elephant(row,col, state.getCurrentTurnColor(), 'E');
-				return elephant.legalMoves(state);
+// 			case 'E':
+// 				Elephant elephant = new Elephant(row,col, state.getCurrentTurnColor(), 'E');
+// 				return elephant.legalMoves(state);
 													
-			case 'L':
-				Lion lion = new Lion(row,col, state.getCurrentTurnColor(),'L');
-				return lion.legalMoves(state);
+// 			case 'L':
+// 				Lion lion = new Lion(row,col, state.getCurrentTurnColor(),'L');
+// 				return lion.legalMoves(state);
 					
-			case 'C':
-				Crocodile crocodile = new Crocodile(row, col, state.getCurrentTurnColor(), 'C');
-				return  crocodile.legalMoves(state);
+// 			case 'C':
+// 				Crocodile crocodile = new Crocodile(row, col, state.getCurrentTurnColor(), 'C');
+// 				return  crocodile.legalMoves(state);
 				
-			case 'Z':
-				Zebra zebra = new Zebra(row,col,state.getCurrentTurnColor(),'Z');
-				return zebra.legalMoves(state);
+// 			case 'Z':
+// 				Zebra zebra = new Zebra(row,col,state.getCurrentTurnColor(),'Z');
+// 				return zebra.legalMoves(state);
 				
-			case 'P':
-				Pawn pawn = new Pawn(row, col, state.getCurrentTurnColor(), 'P');
-				return pawn.legalMoves(state);
+// 			case 'P':
+// 				Pawn pawn = new Pawn(row, col, state.getCurrentTurnColor(), 'P');
+// 				return pawn.legalMoves(state);
 				
-			case 'S':
-				SuperPawn superPawn = new SuperPawn(row, col, state.getCurrentTurnColor(), 'S');
-				return superPawn.legalMoves(state);
+// 			case 'S':
+// 				SuperPawn superPawn = new SuperPawn(row, col, state.getCurrentTurnColor(), 'S');
+// 				return superPawn.legalMoves(state);
 				
-			case 'N':
-					break;  
-			default:
-				System.out.println("Error: Board value "+ state.getPieceSelected().toString() + " not found");	  
-		}
+// 			case 'N':
+// 					break;  
+// 			default:
+// 				System.out.println("Error: Board value "+ state.getPieceSelected().toString() + " not found");	  
+// 		}
 		
-		return moveArray;
+// 		return moveArray;
 
-	}
+// 	}
 
-	// Checks if move is possible based on piece
-	public boolean isMovePossible(State state){
-		int[] newPosition = state.getCurrentClick();
+// 	// Checks if move is possible based on piece
+// 	public boolean isMovePossible(State state){
+// 		int[] newPosition = state.getCurrentClick();
 		
-//		System.out.println("State Value in movePossible: " + state.toString());
-		int[][] possibleMoves;
-		int row = state.getPieceSelected().getRow();
-		int col = state.getPieceSelected().getColumn();
+// //		System.out.println("State Value in movePossible: " + state.toString());
+// 		int[][] possibleMoves;
+// 		int row = state.getPieceSelected().getRow();
+// 		int col = state.getPieceSelected().getColumn();
 
-		switch(state.getPieceSelected().getType()) {
-			case 'G':
-				Giraffe giraffe = new Giraffe(row,col,state.getCurrentTurnColor(),'G');
-				possibleMoves = giraffe.legalMoves(state);
-				return giraffe.containsMove(possibleMoves, newPosition);
+// 		switch(state.getPieceSelected().getType()) {
+// 			case 'G':
+// 				Giraffe giraffe = new Giraffe(row,col,state.getCurrentTurnColor(),'G');
+// 				possibleMoves = giraffe.legalMoves(state);
+// 				return giraffe.containsMove(possibleMoves, newPosition);
 				
-			case 'M':
-				Monkey monkey = new Monkey(row,col,state.getCurrentTurnColor(),'M');
-				possibleMoves = monkey.legalMoves(state);
-				return monkey.containsMove(possibleMoves, newPosition);
+// 			case 'M':
+// 				Monkey monkey = new Monkey(row,col,state.getCurrentTurnColor(),'M');
+// 				possibleMoves = monkey.legalMoves(state);
+// 				return monkey.containsMove(possibleMoves, newPosition);
 					
-			case 'E':
-				Elephant elephant = new Elephant(row,col, state.getCurrentTurnColor(), 'E');
-				possibleMoves = elephant.legalMoves(state);
-				return elephant.containsMove(possibleMoves, newPosition);
+// 			case 'E':
+// 				Elephant elephant = new Elephant(row,col, state.getCurrentTurnColor(), 'E');
+// 				possibleMoves = elephant.legalMoves(state);
+// 				return elephant.containsMove(possibleMoves, newPosition);
 								  					
-			case 'L':
-				Lion lion = new Lion(row,col, state.getCurrentTurnColor(),'L');
-				possibleMoves = lion.legalMoves(state);
-				return lion.containsMove(possibleMoves, newPosition);
+// 			case 'L':
+// 				Lion lion = new Lion(row,col, state.getCurrentTurnColor(),'L');
+// 				possibleMoves = lion.legalMoves(state);
+// 				return lion.containsMove(possibleMoves, newPosition);
 					  
-			case 'C':
-				Crocodile crocodile = new Crocodile(row, col, state.getCurrentTurnColor(), 'C');
-				possibleMoves = crocodile.legalMoves(state);
-				return crocodile.containsMove(possibleMoves, newPosition);
+// 			case 'C':
+// 				Crocodile crocodile = new Crocodile(row, col, state.getCurrentTurnColor(), 'C');
+// 				possibleMoves = crocodile.legalMoves(state);
+// 				return crocodile.containsMove(possibleMoves, newPosition);
 			 	
-			case 'Z':
-				Zebra zebra = new Zebra(row,col,state.getCurrentTurnColor(),'Z');
-				possibleMoves = zebra.legalMoves(state);
-				return zebra.containsMove(possibleMoves, newPosition);
+// 			case 'Z':
+// 				Zebra zebra = new Zebra(row,col,state.getCurrentTurnColor(),'Z');
+// 				possibleMoves = zebra.legalMoves(state);
+// 				return zebra.containsMove(possibleMoves, newPosition);
 			  	
-			case 'P':
-				Pawn pawn = new Pawn(row, col, state.getCurrentTurnColor(), 'P');
-				possibleMoves = pawn.legalMoves(state);
-				return pawn.containsMove(possibleMoves, newPosition);
+// 			case 'P':
+// 				Pawn pawn = new Pawn(row, col, state.getCurrentTurnColor(), 'P');
+// 				possibleMoves = pawn.legalMoves(state);
+// 				return pawn.containsMove(possibleMoves, newPosition);
 
-			case 'S':
-				SuperPawn superPawn = new SuperPawn(row, col, state.getCurrentTurnColor(), 'S');
-				possibleMoves = superPawn.legalMoves(state);
-				return superPawn.containsMove(possibleMoves, newPosition);
-			case 'N':
-				// TODO: Error message or handle somehow
-				System.out.println("Error: Empty tile passed into piece logic");
+// 			case 'S':
+// 				SuperPawn superPawn = new SuperPawn(row, col, state.getCurrentTurnColor(), 'S');
+// 				possibleMoves = superPawn.legalMoves(state);
+// 				return superPawn.containsMove(possibleMoves, newPosition);
+// 			case 'N':
+// 				// TODO: Error message or handle somehow
+// 				System.out.println("Error: Empty tile passed into piece logic");
 				  
-			default:
-				System.out.println("Error: Board value "+ state.getPieceSelected().toString() + " not found");
-				System.exit(1);
-		  }
+// 			default:
+// 				System.out.println("Error: Board value "+ state.getPieceSelected().toString() + " not found");
+// 				System.exit(1);
+// 		  }
 
-		  return false;
+// 		  return false;
 		
-	}
-	
-	// TODO: does this need legalMove check?
-	// Move piece to destination and update GUI 
-	public void movePiece(State state, Piece newPosition){
+// 	}
 
-		// Board that will take over state after move G
-		Piece[][] newBoard = state.getBoard();
+	// // Move piece to destination and update GUI 
+	// public void movePiece(State state, Piece newPosition){
+
+	// 	// Board that will take over state after move G
+	// 	Piece[][] newBoard = state.getBoard();
 		
-		// Set old piece position to null
-		int oldX = state.getPieceSelected().getRow();
-		int oldY = state.getPieceSelected().getColumn();
+	// 	// Set old piece position to null
+	// 	int oldX = state.getPieceSelected().getRow();
+	// 	int oldY = state.getPieceSelected().getColumn();
 
-		newBoard[oldX][oldY] = null;
+	// 	newBoard[oldX][oldY] = null;
 		
-		// Place new piece
-		int newX = newPosition.getRow();
-		int newY = newPosition.getColumn();
+	// 	// Place new piece
+	// 	int newX = newPosition.getRow();
+	// 	int newY = newPosition.getColumn();
 
-		newBoard[newX][newY] = newPosition;
+	// 	newBoard[newX][newY] = newPosition;
 
-		//Update state
-		state.setBoard(newBoard);
-	}
+	// 	//Update state
+	// 	state.setBoard(newBoard);
+	// }
 	
 	
 	// Checks for the three end game conditions
-	public boolean isGameOver(State state){
+	public char isGameOver(State state){
 		Piece[][] board = state.getBoard();
-		boolean returnVar = true;
-		// Lion against lion and at least one piece
+
+		char returnVar = 'A';
+
+		// White Lion and at least one piece against black lion (White Wins)
 		int lionCounter = 0;
 		int pieceCounter = 0;
 		char lionColor = '\0';
-		System.out.println("In gameover");
+
 		for(int i = 0; i < 7; i++) {
 			for(int j = 0; j < 7; j++) {
 				if(board[i][j] != null ){
 					if(board[i][j].getType() == 'L' ) {
-						if(lionColor != '\0') {
-							lionColor = board[i][j].getColor();
-						}
 						lionCounter++;
 					}
 					if( board[i][j].getType() != 'L' && board[i][j].getType() != 'N') { 
-						if(board[i][j].getColor() == lionColor) {
-							returnVar = false;
+						if(board[i][j].getColor() == 'B') {
+							returnVar = 'N';
 						}
 						pieceCounter++;
 					}
 				}
-				if(lionCounter == 2 && pieceCounter >= 1) {
-					return true;
+				if(lionCounter == 2 && pieceCounter >= 1 && returnVar != 'N') {
+					return 'W';
 				}
 			}
 		}
-		System.out.println("Pass");
+
+		// Black Lion and at least one piece against white lion (Black Wins)
+		lionCounter = 0;
+		pieceCounter = 0;
+		lionColor = '\0';
+		returnVar = 'A';
+
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 7; j++) {
+				if(board[i][j] != null ){
+					if(board[i][j].getType() == 'L' ) {
+						lionCounter++;
+					}
+					if( board[i][j].getType() != 'L' && board[i][j].getType() != 'N') { 
+						if(board[i][j].getColor() == 'W') {
+							returnVar = 'N';
+						}
+						pieceCounter++;
+					}
+				}
+				if(lionCounter == 2 && pieceCounter >= 1 && returnVar != 'N') {
+					return 'B';
+				}
+			}
+		}
+
 		// Lion captured 
 		lionCounter = 0;
 		for(int i = 0; i < 7; i++) {
@@ -258,108 +252,37 @@ public class GameLogic extends State{
 				if(board[i][j] != null ){
 					if( board[i][j].getType() == 'L') {
 						lionCounter++;
+						lionColor = board[i][j].getColor();
 					}
 				}
 			}
 		}
 		if(lionCounter == 1) {
-			return true;
+			return lionColor;
 		}
-		System.out.println("Pass");
+
 		// Draw condition. Only two lions left 
 		lionCounter = 0;
+		returnVar = 'A';
+
 		for(int i = 0; i < 7; i++) {
 			for(int j = 0; j < 7; j++) {
-				// If any other piece but lion, return false
 				if(board[i][j] != null) {
 					if(board[i][j].getType() == 'L') {
 						lionCounter++;
 					}else {
-						returnVar = false;
+						returnVar = 'N';
 					}
 				}
 				
 			}
 		} 
-		if(lionCounter == 2) {
-			returnVar = true;
+		if(lionCounter == 2 && returnVar != 'N') {
+			return 'D';
 		}
-		System.out.println("Pass");
-		return returnVar;
+		
+		return 'N';
 	}
-// 	//
-// 	// Main Logic 
-// 	// 
-	
-// 	// Gets called for every click on the board, checks validity of moves and displays possible moves 
-// 	public void mainLogic(State state) {
-		
-// //		System.out.println("In logic");
-		
-// 		// If end turn button is clicked
-// 		if (state.getCurrentClick() == {-1,-1}) {
-// 			if(isGameOver(state)) {
-// 				// TODO: Display some end game message and update database history 
-// 			}
-			
-// 			// If game is not over, end turn and flip board
-// 			state.setBoard(flipBoard(state));
-			
-// 			// TODO: update DB with turn and current state 
-			
-			
-// 			// Only one function call per click 
-// 			return;
-// 		}
-		
-		
-// 		// If piece clicked 
-// 		if (state.getPieceSelected().charAt(2) != 'N' ) {
-			
-// 			// If own piece 
-// 			if (state.getPieceSelected().charAt(2) == state.getCurrentTurnColor().charAt(0)) {
-// 				// Display possible moves 
-// 				displayPossibleMoves(state);
-				
-// 				//Only one function call per click 
-// 				return; 
-// 			}
-		
-// 		// If enemy piece or empty space 	
-// 		if (state.getPieceSelected().charAt(2) != 'N' )
-			
-// 			if(isMovePossible(state)) {
-// 				movePiece(state, state.getCurrentClick());
-				
-// 				//Only one function call per click 
-// 				return; 
-// 			}
-		
-// 		} 
-	
-// 	}
 
-	// Main method for current testing (Remove in production) 
-	public static void main (String[] args){
-		
-		Lion lion1 = new Lion(0,2,'B','L');
-		Pawn pawn1 = new Pawn(1,3,'B','P');
-		Pawn pawn2 = new Pawn(5,1,'W','P');
-		Piece[][] board1 = {
-			{null, null, lion1, null, null, null, null},
-			{null, null, null, pawn1, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, pawn2, null, null, null, null, null},
-			{null, null, null, null, null, null, null}
-		};
-		State state = new State(board1,'W',new int[] {4,1},pawn1);
-		GameLogic logic = new GameLogic();
-		System.out.println(logic.isGameOver(state));
-		
-		
-		System.out.println("Done");
-//		System.out.print(state.toString());
-	}
+
 }
--- Congo/Tests/GameLogicTest.java
+++ Congo/Tests/GameLogicTest.java
@@ -2,30 +2,16 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import javax.swing.plaf.synth.SynthLookAndFeel;
+
 import org.junit.Test;
 
 import BoardLogic.*;
 
 public class GameLogicTest {
 	
-	GameLogic logicTest;
-// 	@Test
-// 	public void testIsIndexBoundedForUnBoundedValues() {
-// 		GameLogic gameLogic=new GameLogic();
-// 		int i=7;
-// 		int j=-1;
-// 		assertFalse(gameLogic.isIndexBounded(i,j));
-// 	}
-// 	@Test
-// 	public void testIsOnCorrectIndex() {
-// 		int x=2;
-// 		int y= 1;
-// 		boolean row=true;
-// 		boolean column=false;
-// 		GameLogic gameLogic=new GameLogic();
-//         assertTrue(gameLogic.isOnCorrectIndex(x, y, row, column));
-// 	}
-//     @Test
+	GameLogic logic = new GameLogic();
+
 //     public void testContainsMoveWhenTrue() {
 //     	String[] possibleMoves= {"11BP","10BE","21BC"};
 //     	String newPosition="10BE";
@@ -39,90 +25,30 @@ public class GameLogicTest {
 // 		GameLogic gameLogic=new GameLogic();
 //         assertFalse(gameLogic.containsMove(possibleMoves, newPosition));   	
 //     }
+ 
 
+	
 
 	// @Test
-	// public void testFlipBoard() {
-	
-	// 	// White Pieces 
-	// 	Pawn pawnW1 = new Pawn(5,0,'W','P');
-	// 	Pawn pawnW2 = new Pawn(5,1,'W','P');
-	// 	Pawn pawnW3 = new Pawn(5,2,'W','P');
-	// 	Pawn pawnW4 = new Pawn(5,3,'W','P');
-	// 	Pawn pawnW5 = new Pawn(5,4,'W','P');
-	// 	Pawn pawnW6 = new Pawn(5,5,'W','P');
-	// 	Pawn pawnW7 = new Pawn(5,6,'W','P');
-
-	// 	Giraffe giraffeW = new Giraffe(6, 0, 'W', 'G');
-	// 	Monkey monkeyW = new Monkey(6,1,'W','M');
-	// 	Elephant elephantW1 = new Elephant(6, 2, 'W', 'E');
-	// 	Lion lionW = new Lion(6, 3, 'W', 'L');
-	// 	Elephant elephantW2 = new Elephant(6, 4, 'W', 'E');
-	// 	Crocodile crocodileW = new Crocodile(6, 5, 'W', 'C');
-	// 	Zebra zebraW = new Zebra(6, 6, 'W', 'Z');
-
-	// 	// Black Pieces 
-	// 	Pawn pawnB1 = new Pawn(1,0,'B','P');
-	// 	Pawn pawnB2 = new Pawn(1,1,'B','P');
-	// 	Pawn pawnB3 = new Pawn(1,2,'B','P');
-	// 	Pawn pawnB4 = new Pawn(1,3,'B','P');
-	// 	Pawn pawnB5 = new Pawn(1,4,'B','P');
-	// 	Pawn pawnB6 = new Pawn(1,5,'B','P');
-	// 	Pawn pawnB7 = new Pawn(1,6,'B','P');
-
-	// 	Giraffe giraffeB = new Giraffe(6, 0, 'B', 'G');
-	// 	Monkey monkeyB = new Monkey(6,1,'B','M');
-	// 	Elephant elephantB1 = new Elephant(6, 2, 'B', 'E');
-	// 	Lion lionB = new Lion(6, 3, 'B', 'L');
-	// 	Elephant elephantB2 = new Elephant(6, 4, 'B', 'E');
-	// 	Crocodile crocodileB = new Crocodile(6, 5, 'B', 'C');
-	// 	Zebra zebraB = new Zebra(6, 6, 'B', 'Z');
-		
-
-	// 	Piece[][] board1 = {
-	// 		{giraffeB, monkeyB, elephantB1, lionB, elephantB2, crocodileB, zebraB},
-	// 		{pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7},
+    // public void testDisplayPossibleMoves() {
+	// 	Pawn pawn = new Pawn(5,1,'W','P');
+	// 	Piece[][] board = {
 	// 		{null, null, null, null, null, null, null},
 	// 		{null, null, null, null, null, null, null},
 	// 		{null, null, null, null, null, null, null},
-	// 		{pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7},
-	// 		{giraffeW, monkeyW, elephantW1, lionW, elephantW2, crocodileW, zebraW}
+	// 		{null, null, null, null, null, null, null},
+	// 		{null, null, null, null, null, null, null},
+	// 		{null, pawn, null, null, null, null, null},
+	// 		{null, null, null, null, null, null, null}
 	// 	};
+	// 	State state = new State(board,'W',new int[] {4,1},pawn);
+	// 	int[][] expectedMoves = new int[20][2];
+	// 	expectedMoves[0][0] = 4; expectedMoves[0][1] = 0; 
+	// 	expectedMoves[1][0] = 4; expectedMoves[1][1] = 1; 
+	// 	expectedMoves[2][0] = 4; expectedMoves[2][1] = 2; 
 
-	// 	Piece[][] board2 = {
-	// 		{giraffeW, monkeyW, elephantW1, lionW, elephantW2, crocodileW, zebraW},
-	// 		{pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{giraffeB, monkeyB, elephantB1, lionB, elephantB2, crocodileB, zebraB},
-	// 		{pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7}
-	// 	};
-
-		
-	// 	assertTrue(logicTest.boardEquals(board1,board2));
+	// 	assertArrayEquals(expectedMoves, pawn.legalMoves(state));
 	// }
-
-	@Test
-    public void testDisplayPossibleMoves() {
-		Pawn pawn = new Pawn(5,1,'W','P');
-		Piece[][] board = {
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, null, null, null, null, null, null},
-			{null, pawn, null, null, null, null, null},
-			{null, null, null, null, null, null, null}
-		};
-		State state = new State(board,'W',new int[] {4,1},pawn);
-		int[][] expectedMoves = new int[20][2];
-		expectedMoves[0][0] = 4; expectedMoves[0][1] = 0; 
-		expectedMoves[1][0] = 4; expectedMoves[1][1] = 1; 
-		expectedMoves[2][0] = 4; expectedMoves[2][1] = 2; 
-
-		assertArrayEquals(expectedMoves, pawn.legalMoves(state));
-	}
 
 	//
 	// Test isMovePossible method 
@@ -194,11 +120,13 @@ public class GameLogicTest {
 	//
 	// Test isGameOver method 
 	//
+
     @Test
-    public void testIsGameOverWhenTrue1() {
+    public void testIsGameOverWhenBlackWins1() {
 		Lion lion1 = new Lion(0,2,'B','L');
 		Pawn pawn1 = new Pawn(1,3,'B','P');
 		Pawn pawn2 = new Pawn(5,1,'W','P');
+
 		Piece[][] board1 = {
 			{null, null, lion1, null, null, null, null},
 			{null, null, null, pawn1, null, null, null},
@@ -208,65 +136,136 @@ public class GameLogicTest {
 			{null, pawn2, null, null, null, null, null},
 			{null, null, null, null, null, null, null}
 		};
-		State state = new State(board1,'W',new int[] {4,1},pawn1);
-		System.out.println("IS OVER: "+ state.toString());
-        assertTrue(logicTest.isGameOver(state));
+		State state = new State(board1,'B',new int[] {4,1},pawn1);
+
+		char expectedVar = 'B';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
+
+        assertEquals(expectedVar, resultVar);
 	}
 
-	// @Test
-    // public void testIsGameOverWhenTrue2() {
-	// 	Lion lion1 = new Lion(0,2,'B','L');
-	// 	Lion lion2 = new Lion(6,2,'W','L');
-	// 	Pawn pawn2 = new Pawn(5,1,'W','P');
-	// 	Piece[][] board1 = {
-	// 		{null, null, lion1, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, pawn2, null, null, null, null, null},
-	// 		{null, null, lion2, null, null, null, null}
-	// 	};
-	// 	State state = new State(board1,'W',new int[] {4,1},pawn2);
-    //     assertTrue(logicTest.isGameOver(state));
-	// }
+	@Test
+    public void testIsGameOverWhenBlackWins2() {
+		Lion lion1 = new Lion(0,2,'B','L');
+		Pawn pawn1 = new Pawn(1,3,'B','P');
+		Lion lion2 = new Lion(6,2,'W','L');
 
-	// @Test
-    // public void testIsGameOverWhenFalse1() {
-	// 	Lion lion1 = new Lion(0,2,'B','L');
-	// 	Lion lion2 = new Lion(6,2,'W','L');
-	// 	Pawn pawn1 = new Pawn(1,3,'B','P');
-	// 	Pawn pawn2 = new Pawn(5,1,'W','P');
-	// 	Piece[][] board1 = {
-	// 		{null, null, lion1, null, null, null, null},
-	// 		{null, null, null, pawn1, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, pawn2, null, null, null, null, null},
-	// 		{null, null, lion2, null, null, null, null}
-	// 	};
-	// 	State state = new State(board1,'W',new int[] {4,1},pawn1);
-    //     assertFalse(logicTest.isGameOver(state));
-	// }
+		Piece[][] board1 = {
+			{null, null, lion1, null, null, null, null},
+			{null, null, null, pawn1, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, lion2, null, null, null, null}
+		};
+		State state = new State(board1,'B',new int[] {4,1},pawn1);
 
-	// @Test
-    // public void testIsGameOverWhenDraw() {
-	// 	Lion lion1 = new Lion(0,2,'B','L');
-	// 	Lion lion2 = new Lion(6,2,'W','L');
+		char expectedVar = 'B';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
 
-	// 	Piece[][] board1 = {
-	// 		{null, null, lion1, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, null, null, null, null, null},
-	// 		{null, null, lion2, null, null, null, null}
-	// 	};
-	// 	State state = new State(board1,'W',new int[] {4,1},lion1);
-    //     assertTrue(logicTest.isGameOver(state));
-	// }
+        assertEquals(expectedVar, resultVar);
+	}
+
+	@Test
+    public void testIsGameOverWhenWhiteWins1() {
+		Pawn pawn1 = new Pawn(1,3,'B','P');
+		Lion lion2 = new Lion(6,2,'W','L');
+		Pawn pawn2 = new Pawn(5,1,'W','P');
+
+		Piece[][] board1 = {
+			{null, null, null, null, null, null, null},
+			{null, null, null, pawn1, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, pawn2, null, null, null, null, null},
+			{null, null, lion2, null, null, null, null}
+		};
+		State state = new State(board1,'B',new int[] {4,1},pawn1);
+
+		char expectedVar = 'W';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
+
+        assertEquals(expectedVar, resultVar);
+	}
+
+	@Test
+    public void testIsGameOverWhenWhiteWins2() {
+		Lion lion1 = new Lion(0,2,'B','L');
+		Pawn pawn1 = new Pawn(1,3,'B','P');
+		Lion lion2 = new Lion(6,2,'W','L');
+		Pawn pawn2 = new Pawn(5,1,'W','P');
+
+		Piece[][] board1 = {
+			{null, null, lion1, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, pawn2, null, null, null, null, null},
+			{null, null, lion2, null, null, null, null}
+		};
+		State state = new State(board1,'B',new int[] {4,1},pawn1);
+
+		char expectedVar = 'W';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
+
+        assertEquals(expectedVar, resultVar);
+	}
+
+	@Test
+    public void testIsGameOverWhenDraw() {
+		Lion lion1 = new Lion(0,2,'B','L');
+		Lion lion2 = new Lion(6,2,'W','L');
+
+		Piece[][] board1 = {
+			{null, null, lion1, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, lion2, null, null, null, null}
+		};
+		State state = new State(board1,'B',new int[] {4,1},lion1);
+
+		char expectedVar = 'D';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
+
+        assertEquals(expectedVar, resultVar);
+	}
+
+	@Test
+    public void testIsGameOverWhenNull() {
+		Lion lion1 = new Lion(0,2,'B','L');
+		Pawn pawn1 = new Pawn(1,1,'B','P');
+		Lion lion2 = new Lion(6,2,'W','L');
+		Pawn pawn2 = new Pawn(5,1,'W','P');
+
+		Piece[][] board1 = {
+			{null, null, lion1, null, null, null, null},
+			{null, pawn1, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, null, null, null, null, null, null},
+			{null, pawn2, null, null, null, null, null},
+			{null, null, lion2, null, null, null, null}
+		};
+		State state = new State(board1,'B',new int[] {4,1},lion1);
+
+		char expectedVar = 'N';
+		char resultVar = logic.isGameOver(state);
+		System.out.println("Expected Var: " + expectedVar + " Result Var: " + resultVar);
+
+        assertEquals(expectedVar, resultVar);
+	}
+
 	
 }
     	
